b9068d4e499d3d882aba79b897cd8d97d07cb623
政翰 林 <4a827013@office.stust.edu.tw>
2022-09-19 21:13:34 +0800

    20220919

---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        MsgBox("hello world")
+        Dim num As Integer = 100
+        MsgBox(num)
+        Console.WriteLine("4A827013")
+    End Sub
+
+End Module
